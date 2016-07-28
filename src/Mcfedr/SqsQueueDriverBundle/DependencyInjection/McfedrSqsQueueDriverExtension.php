25473e8fbe89bb103a525bba4fa776d9d5a107b1
Fred Cox <fred@ekreative.com>
2016-07-28 09:09:44 +0300

    Remove unused code, connect to released version of queue-manager

--- src/Mcfedr/SqsQueueDriverBundle/DependencyInjection/McfedrSqsQueueDriverExtension.php
+++ src/Mcfedr/SqsQueueDriverBundle/DependencyInjection/McfedrSqsQueueDriverExtension.php
@@ -22,11 +22,6 @@ class McfedrSqsQueueDriverExtension extends Extension implements PrependExtensio
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
     }
 
     /**
