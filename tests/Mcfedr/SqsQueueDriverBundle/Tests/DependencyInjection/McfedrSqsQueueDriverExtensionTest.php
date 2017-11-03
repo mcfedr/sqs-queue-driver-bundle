a8a2a3e1bfa8426cc4c534ecd7eb0c505f7018f5
Fred Cox <frederick.j.cox@gmail.com>
2017-11-03 15:38:53 +0200

    Now with new class name service

--- tests/Mcfedr/SqsQueueDriverBundle/Tests/DependencyInjection/McfedrSqsQueueDriverExtensionTest.php
+++ tests/Mcfedr/SqsQueueDriverBundle/Tests/DependencyInjection/McfedrSqsQueueDriverExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace Mcfedr\SqsQueueDriverBundle\Tests\DependencyInjection;
 
+use Mcfedr\SqsQueueDriverBundle\Manager\SqsQueueManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class McfedrSqsQueueDriverExtensionTest extends WebTestCase
@@ -9,6 +10,7 @@ class McfedrSqsQueueDriverExtensionTest extends WebTestCase
     public function testConfiguration()
     {
         $client = static::createClient();
+        $this->assertTrue($client->getContainer()->has(SqsQueueManager::class));
         $this->assertTrue($client->getContainer()->has('mcfedr_queue_manager.default'));
     }
 }
